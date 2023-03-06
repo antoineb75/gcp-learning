9dcefef6fa27035a8bf5e38057231ead8187fa25
Antoine B <abaret73@gmail.com>
2023-03-06 22:59:37 +0100

    bon ca marche tant qu on va pas sur pubsub dans le navig

--- pubsub-publisher.py
+++ pubsub-publisher.py
@@ -1,4 +1,5 @@
 import os
+import time
 from google.cloud import pubsub_v1
 
 credentials_path = 'tunes-admin.privateKey.json'
@@ -7,14 +8,15 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 publisher = pubsub_v1.PublisherClient()
 topic_path = 'projects/prepa-certif-pca/topics/music-tunes'
 
+timestamp = str(time.time())
 data = 'A new tune is ready'
 data = data.encode('utf-8')
 attributes = {
     'artist': 'beatles',
     'album': 'white album',
-    'title': 'blackbird6'
+    'title': 'song '+str(time.time())
 }
 
 future = publisher.publish(topic_path, data, **attributes)
 
-print(f'published message id {future.result()}')
+print(f'for timestamp {timestamp} published message id {future.result()}')
